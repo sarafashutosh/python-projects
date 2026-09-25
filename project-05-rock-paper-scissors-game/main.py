5f03f9c6cc61982545b1e8c5e49ae81fbbe52b2a
sarafashutosh <ashutosh.saraf15@gmail.com>
2026-09-24 23:46:27 -0400

    Project 5: Rock Paper Scissors Game

--- project-05-rock-paper-scissors-game/main.py
+++ project-05-rock-paper-scissors-game/main.py
@@ -30,20 +30,19 @@ print ("Welcome to the game of Rock, Paper, Scissors!\n")
 user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 if (user_choice>=0 and user_choice<=2):
     print("You choose:\n" + sign_image[user_choice])
-computer_choice=random.randint(0,2)
-print("Computer chose:\n" + sign_image[computer_choice])
-
-if (user_choice <0 or user_choice >=3):
-    print("You typed an invalid option. You lose!")
-elif(user_choice== 0 and computer_choice==2):
-    print("You win!")
-elif (user_choice < computer_choice):
-    print("You lose!")
-elif (computer_choice == user_choice):
-    print("It's a draw!")
-elif (computer_choice == 0 and user_choice == 2):
-    print("You lose!")
-elif (computer_choice < user_choice):
-    print("You win!")
+    computer_choice=random.randint(0,2)
+    print("Computer chose:\n" + sign_image[computer_choice])
+    if(user_choice== 0 and computer_choice==2):
+        print("You win!")
+    elif (user_choice < computer_choice):
+        print("You lose!")
+    elif (computer_choice == user_choice):
+        print("It's a draw!")
+    elif (computer_choice == 0 and user_choice == 2):
+        print("You lose!")
+    elif (computer_choice < user_choice):
+        print("You win!")
+else:
+    print("You typed an invalid option. Please try again.")
 
 
